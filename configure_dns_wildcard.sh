142f2fd3f074316103c0834bbcbd32c8b5a82bc5
Dylan Murray <dymurray@redhat.com>
2017-03-23 15:02:07 -0400

    Added script to update dnsmasq with wildcard

---
+++ configure_dns_wildcard.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You must be a root user to run" 2>&1
+    exit 1
+fi
+
+service NetworkManager stop
+
+grep -q -F 'dns=dnsmasq' /etc/NetworkManager/NetworkManager.conf || sed -i '/\[main\]/a dns=dnsmasq' /etc/NetworkManager/NetworkManager.conf
+mkdir -p /etc/NetworkManager/dnsmasq.d
+touch /etc/NetworkManager/dnsmasq.d/local-development.conf
+grep -q -F 'address=/cloudapps' /etc/NetworkManager/dnsmasq.d/local-development.conf || \
+    echo "address=/apps.example.com/192.168.156.5" >> /etc/NetworkManager/dnsmasq.d/local-development.conf
+
+service NetworkManager start
